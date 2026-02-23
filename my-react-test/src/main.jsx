803d46d33953545f6a0909192cdde69a3559cb53
Rob Shiels <rob@robshiels.com>
2026-02-23 21:12:30 +0000

    First React lesson from Fullstackopen

--- my-react-test/src/main.jsx
+++ my-react-test/src/main.jsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 
 // Import our main App component
-import { App } from './App.jsx'
+import App from './App.jsx'
 
 // Find the 'root' div in index.html and render our App component into it
 createRoot(document.getElementById('root')).render(<App />)
