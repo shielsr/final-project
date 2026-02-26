406ccbb24f12f385c9fee4388487c96d1fa4c0b6
Rob Shiels <rob@robshiels.com>
2026-02-26 16:39:11 +0000

    Timer in Recorder component

--- my-react-test/src/components/recorder.jsx
+++ my-react-test/src/components/recorder.jsx
@@ -16,11 +16,19 @@ export default function Recorder() {
         setIsRecording(false)
     }
 
+    const formatTime = (totalSeconds) => {
+        const hours = Math.floor(totalSeconds / 3600)
+        const minutes = Math.floor((totalSeconds % 3600)/60)
+        const secs = totalSeconds % 60
+
+        return `${String(hours).padStart(2, "0")} : ${String(minutes).padStart(2,"0")} : ${String(secs).padStart(2,"0")}`
+    }
+
     return (
         <div>
 
             <h2>
-               00:00:00 
+               {formatTime(seconds)} 
             </h2>
 
             {isRecording ?
