2015a54da4dc425918a401d1b38f68e2aa0573b5
Rob Shiels <rob@robshiels.com>
2026-02-26 16:31:02 +0000

    Record and stop buttons switch on click in Recorder component

--- my-react-test/src/components/recorder.jsx
+++ my-react-test/src/components/recorder.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaCircleStop, FaMicrophone } from 'react-icons/fa6'
 
 export default function Recorder() {
+
+    const [isRecording, setIsRecording] = useState(false)
+    const [seconds, setSeconds] = useState(0)
+    const startRecording = async() => {
+        setIsRecording(true)
+        const time = setInterval(() => {
+            setSeconds(prev => prev +1)
+        }, 1000)
+    }
+
+    const stopRecording = () => {
+        setIsRecording(false)
+    }
+
     return (
         <div>
 
@@ -9,13 +23,16 @@ export default function Recorder() {
                00:00:00 
             </h2>
 
-            <button>
-                <FaCircleStop />
-            </button>
+            {isRecording ?
+                <button onClick={stopRecording}>
+                    <FaCircleStop />
+                </button> :
+            
 
-            <button>
-                <FaMicrophone />
-            </button>
+                <button onClick={startRecording}>
+                    <FaMicrophone />
+                </button>
+            }
         </div>
     )
 }
