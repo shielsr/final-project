43150c3b291e7bf1817275d1cf91cc0e8cdc188d
Rob Shiels <rob@robshiels.com>
2026-03-29 00:12:41 +0000

    Fixed refresh bug

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,6 +25,9 @@ from django.views.static import serve
 from rest_framework import routers
 from overnote import views
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
 router = routers.DefaultRouter()
